ab08d7e9b4eebbae44b7d5ef93b23b1e889b4cd7
Christian Mørup <chrmorup@gmail.com>
2020-05-19 12:02:36 +0200

    Fixed sweeping jammer

--- contiki-ng-develop/examples/jammer/sweeping_jammer.c
+++ contiki-ng-develop/examples/jammer/sweeping_jammer.c
@@ -1,43 +1,3 @@
-/*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *       Improved example of an interfering node
- * \author
- *        Sergio Diaz
- */
-
-/*INCLUDES*/
 #include "contiki.h"
 #include "dev/cc2420/cc2420.h"
 #include "dev/cc2420/cc2420_const.h" // Include the CC2420 constants
@@ -51,11 +11,11 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 /*DEFINES*/
-#define CONSTANT_MICROS 300 // 300 us was defined by the paper [1]
-#define TIME_TICK 31        // The time of 1 tick is 30,51 us in rtimer for sky motes in Contiki v3.0. TIME_TICK = 1 / RTIMER_ARCH_SECOND = 1 / 32768 s
+#define CONSTANT_MICROS 10 // 300 us was defined by the paper [1]
+#define TIME_TICK 31       // The time of 1 tick is 30,51 us in rtimer for sky motes in Contiki v3.0. TIME_TICK = 1 / RTIMER_ARCH_SECOND = 1 / 32768 s
 
 static struct rtimer rtimer; // Create the rtimer variable
-uint32_t counter;
+
 /*---------------------------------------------------------------------------*/
 /** 
  * Writes to a register.
@@ -90,24 +50,26 @@ static void carrier_OnOff(struct rtimer *timer, void *ptr)
 {
     uint32_t num_ticks; // Number of ticks of the time_next_period
 
-    if (counter < 42949 && cc2420_get_channel() < 26)
+    if (cc2420_get_channel() < 26)
     {
         cc2420_set_channel(cc2420_get_channel() + 1);
-        counter++;
     }
     else
     {
         cc2420_set_channel(11);
-        counter = 0;
     }
 
     //The node must generate interference. Turn the carrier on.
     // Creates an unmodulated carrier by setting the appropiate registers in the CC2420
-    setreg(CC2420_MANOR, 0x0100);
-    setreg(CC2420_TOPTST, 0x0004);
-    setreg(CC2420_MDMCTRL1, 0x0508);
-    setreg(CC2420_DACTST, 0x1800);
-    strobe(CC2420_STXON);
+    int i;
+    for (i = 0; i < 50; i++)
+    {
+        setreg(CC2420_MANOR, 0x0100);
+        setreg(CC2420_TOPTST, 0x0004);
+        setreg(CC2420_MDMCTRL1, 0x0508);
+        setreg(CC2420_DACTST, 0x1800);
+        strobe(CC2420_STXON);
+    }
 
     // Set the rtimer to the time_next_period (num_ticks) by compute the next time period according to the paper [1]
     num_ticks = 10 * CONSTANT_MICROS / TIME_TICK;
@@ -122,7 +84,7 @@ AUTOSTART_PROCESSES(&turn_carrier_OnOff);           // Load the process on boot
 
 PROCESS_THREAD(turn_carrier_OnOff, ev, data) // Process to turn carrier on and off
 {
-    counter = 0;
+
     PROCESS_BEGIN(); // Says where the process starts
 
     rtimer_set(&rtimer, RTIMER_NOW() + RTIMER_ARCH_SECOND, 1, carrier_OnOff, NULL); //Initiates the rtimer 1 second after boot
--- contiki-ng-develop/examples/wsn-project2/client.c
+++ contiki-ng-develop/examples/wsn-project2/client.c
@@ -1,43 +1,3 @@
-/*
- * Copyright (c) 2017, RISE SICS.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         NullNet broadcast example
- * \author
-*         Simon Duquennoy <simon.duquennoy@ri.se>
- *
- */
-
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
@@ -62,8 +22,8 @@ static linkaddr_t coordinator_addr = {{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 #endif /* MAC_CONF_WITH_TSCH */
 
 /*---------------------------------------------------------------------------*/
-PROCESS(nullnet_example_process, "NullNet broadcast example");
-AUTOSTART_PROCESSES(&nullnet_example_process);
+PROCESS(jamming_project_client, "Jamming Project Client");
+AUTOSTART_PROCESSES(&jamming_project_client);
 
 /*---------------------------------------------------------------------------*/
 static unsigned long
@@ -88,7 +48,7 @@ void input_callback(const void *data, uint16_t len,
 }
 
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(nullnet_example_process, ev, data)
+PROCESS_THREAD(jamming_project_client, ev, data)
 {
   static struct etimer alive_timer;
   static struct etimer event_timer;
